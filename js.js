9191c4fe1833099700e55737f29d1ebfdec2d189
Willie Thompson <willielaredo@gmail.com>
2023-06-10 17:07:29 -0400

    first commit

---
+++ js.js
@@ -0,0 +1,41 @@
+// show/hide filter options
+
+var button = document.getElementById('toggle-filters'); // Assumes element with id='button'
+
+button.onclick = function() {
+    var div = document.getElementById('filters');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    }
+    else {
+        div.style.display = 'block';
+    }
+};
+
+// create cards
+fetch('./orgs.json')
+    .then((response) => response.json())
+    .then((json) => {
+        const arr = json.data
+        const countSpan = document.getElementById("count");
+        const newText = document.createTextNode(arr.length);
+        countSpan.appendChild(newText)
+
+        for (var i = 0; i < arr.length; i++) {
+            var obj = arr[i];
+            const name = obj.name
+            const description = obj.short_description
+            const profile_tags = obj.tags
+            const profile_tags_str = profile_tags.toString()
+            console.log(profile_tags)
+            
+            // for (const y in profile_tags) {
+            //     console.log(y[0])
+            // }
+            const card = document.createElement("article")
+            card.innerHTML = 
+                '<header><h2>' + name + '</h2></header><div class="content"><p>' + description + '</p></div><footer><div class="tag">' + profile_tags_str.replaceAll(",",'</div><div class="tag">') + '</div></footer>'
+                    // .replaceAll(',', '</div><div>') + '</div>
+            document.getElementById('cards').appendChild(card)
+        }
+    });

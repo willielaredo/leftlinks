6db0560ca4095071a1ca76180981f500fee7e342
Willie Thompson <willielaredo@gmail.com>
2023-06-11 18:49:04 -0400

    add images to cards

--- js.js
+++ js.js
@@ -24,13 +24,14 @@ fetch('./orgs.json')
         for (var i = 0; i < arr.length; i++) {
             var obj = arr[i];
             const name = obj.name
-            const slug = obj.slug ? obj.slug : 'placeholder'
             const description = obj.short_description
+            const slug = obj.slug
             const profile_tags = obj.tags
+            const screen_name = obj.screen_name
             const profile_tags_str = profile_tags.toString()
             const card = document.createElement("article")
             card.innerHTML = 
-                '<header><h2>' + name + '</h2></header><img src="/images/' + slug + '.png" /><div class="content"><p>' + description + '</p></div><footer><div class="tag">' + profile_tags_str.replaceAll(",",'</div><div class="tag">') + '</div></footer>'
+                '<header><h2>' + name + '</h2></header><img src="/images/twit_pics/' + screen_name + '.jpg    " /><div class="content"><p>' + description + '</p></div><footer><div class="tag">' + profile_tags_str.replaceAll(",",'</div><div class="tag">') + '</div></footer>'
                     // .replaceAll(',', '</div><div>') + '</div>
             document.getElementById('cards').appendChild(card)
         }

7f0816a2ae77648edd9c43f61346b6487081198c
Willie Thompson <willielaredo@gmail.com>
2023-06-28 00:45:44 -0400

    add web and twitter links to cards

--- js.js
+++ js.js
@@ -26,13 +26,14 @@ fetch('./orgs.json')
             const name = obj.name
             const description = obj.short_description
             const slug = obj.slug
+            const url = obj.url
             const profile_tags = obj.tags
             const screen_name = obj.screen_name
             const profile_tags_str = profile_tags.toString()
             const card = document.createElement("article")
             card.classList.add("card")
             card.innerHTML = 
-                '<div><h2>' + name + '</h2></div><div class="card-image"><img src="./images/twit_pics/' + screen_name + '.jpg" /></div><div class="content"><p>' + description + '</p></div><footer><div class="tag">' + profile_tags_str.replaceAll(",",'</div><div class="tag">') + '</div></footer>'
+                '<div><h2>' + name + '</h2></div> <div class="card-image"><img src="./images/twit_pics/' + screen_name + '.jpg" /></div> <div class="content"><p>' + description + '</p></div> <div class="tags"><div class="tag">' + profile_tags_str.replaceAll(",",'</div><div class="tag">') + '</div> </div><div class="links-container"> <a href="' + url + '"><div class="links"> <i class="fa-light fa-arrow-up-right-from-square"></i>Website </div></a> <a href="https://twitter.com/' + screen_name + '"</a><div class="links"> Twitter </div> </div>'
                     // .replaceAll(',', '</div><div>') + '</div>
             document.getElementById('cards').appendChild(card)
         }

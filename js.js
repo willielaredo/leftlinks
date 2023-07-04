ef423b09852ca6d4f16c9b6f96f9ebd7ed9c5d0b
Willie Thompson <willielaredo@gmail.com>
2023-07-04 18:06:43 -0400

    add tag list to homepage display tag list if search is empty

--- js.js
+++ js.js
@@ -19,96 +19,125 @@ fetch('./orgs.json')
         const arr = json.data
         const countSpan = document.getElementById("count");
         const newText = document.createTextNode(arr.length);
+        const org_list = document.getElementById('cards')
+        const issues_list = document.getElementById('issues')
         countSpan.appendChild(newText)
-        function renderAllCards() {
+        // function renderAllCards() {
+        //     for (var i = 0; i < arr.length; i++) {
+        //         var obj = arr[i];
+        //         const name = obj.name
+        //         const description = obj.short_description
+        //         const slug = obj.slug
+        //         const url = obj.url
+        //         const profile_tags = obj.tags
+        //         const screen_name = obj.screen_name
+        //         const profile_tags_str = profile_tags.toString()
+        //         const card = document.createElement("article")
+        //         card.classList.add("card")
+        //         if (profile_tags) { card.classList.add(...profile_tags) }
+        //         const nameDiv = document.createElement("div")
+        //         const imgDiv = document.createElement("div")
+        //         const descDiv = document.createElement("div")
+        //         const tagsDiv = document.createElement("div")
+        //         const linksDiv = document.createElement("div")
+        //         const websiteDiv = document.createElement("div")
+        //         const twitterDiv = document.createElement("div")
+        //         const newh2 = document.createElement("h2")
+        //         const newimg = document.createElement("img")
+        //         const website = document.createElement("a")
+        //         const twitter = document.createElement("a")
+        //         const newP = document.createElement("p")
+        //         card.append(nameDiv, imgDiv, descDiv, tagsDiv, linksDiv)
+        //         tagsDiv.classList.add("tags")
+        //         nameDiv.appendChild(newh2).textContent += name
+        //         newh2.classList.add("card-title")
+        //         imgDiv.appendChild(newimg)
+        //         imgDiv.classList.add("card-image")
+        //         newimg.setAttribute("src", "./images/twit_pics/" + screen_name + ".jpeg")
+        //         descDiv.appendChild(newP).textContent += description
+        //         descDiv.classList.add("content")
+        //         linksDiv.append(website, twitter)
+        //         linksDiv.classList.add("links-container")
+        //         website.setAttribute("href", url)
+        //         website.appendChild(websiteDiv).textContent += "Website"
+        //         websiteDiv.classList.add("links")
+        //         twitter.appendChild(twitterDiv).textContent += "Twitter"
+        //         twitter.setAttribute("href", "https://twitter.com/" + screen_name)
+        //         twitterDiv.classList.add("links")
+        //         profile_tags.forEach(element => {
+        //             if (element) { tagsDiv.innerHTML += '<a href="/?tag=' + element + '"><div class="tag">' + element + '</div></a>' }
+        //         })
+        //         document.getElementById('cards').appendChild(card)
+        //     }
+        // }
+        // renderAllCards();
+        function fetchTagsFromOrgs() {
+            const tag_list = []
             for (var i = 0; i < arr.length; i++) {
                 var obj = arr[i];
-                const name = obj.name
-                const description = obj.short_description
-                const slug = obj.slug
-                const url = obj.url
-                const profile_tags = obj.tags
-                const screen_name = obj.screen_name
-                const profile_tags_str = profile_tags.toString()
-                const card = document.createElement("article")
-                card.classList.add("card")
-                if (profile_tags) { card.classList.add(...profile_tags) }
-                const nameDiv = document.createElement("div")
-                const imgDiv = document.createElement("div")
-                const descDiv = document.createElement("div")
-                const tagsDiv = document.createElement("div")
-                const linksDiv = document.createElement("div")
-                const websiteDiv = document.createElement("div")
-                const twitterDiv = document.createElement("div")
-                const newh2 = document.createElement("h2")
-                const newimg = document.createElement("img")
-                const website = document.createElement("a")
-                const twitter = document.createElement("a")
-                const newP = document.createElement("p")
-                card.append(nameDiv, imgDiv, descDiv, tagsDiv, linksDiv)
-                tagsDiv.classList.add("tags")
-                nameDiv.appendChild(newh2).textContent += name
-                newh2.classList.add("card-title")
-                imgDiv.appendChild(newimg)
-                imgDiv.classList.add("card-image")
-                newimg.setAttribute("src", "./images/twit_pics/" + screen_name + ".jpeg")
-                descDiv.appendChild(newP).textContent += description
-                descDiv.classList.add("content")
-                linksDiv.append(website, twitter)
-                linksDiv.classList.add("links-container")
-                website.setAttribute("href", url)
-                website.appendChild(websiteDiv).textContent += "Website"
-                websiteDiv.classList.add("links")
-                twitter.appendChild(twitterDiv).textContent += "Twitter"
-                twitter.setAttribute("href", "https://twitter.com/" + screen_name)
-                twitterDiv.classList.add("links")
-                profile_tags.forEach(element => {
-                    if (element) { tagsDiv.innerHTML += '<a href="/?tag=' + element + '"><div class="tag">' + element + '</div></a>' }
-                })
-                document.getElementById('cards').appendChild(card)
+                const tags = obj.tags
+                if (tags) {
+                    tags.forEach((tag) => tag_list.push(tag))
+                }
             }
-        }
-        renderAllCards();
+            const unique_tags = tag_list.filter((value, index, array) => array.indexOf(value) === index);
+            unique_tags.sort().forEach((tag) => {
+                const issue = document.createElement("div")
+                issue.classList.add("issue")
+                issue.innerHTML =
+                    '<a href="/?tag=' + tag + '"><h3>#' + tag + '</h3></a>'
+                issues_list.appendChild(issue)
+            });
+            };
+        fetchTagsFromOrgs()
+
+        // render issue page
         function filterCards() {
             var params = new Proxy(new URLSearchParams(window.location.search), {
             get: (searchParams, prop) => searchParams.get(prop),
             });
             var tag = params.tag;
             if (tag) {
-                const matching = document.getElementsByClassName(tag)
-                for (let match of matching) { match.classList.remove("is-hidden") }
-                const not_matching = document.querySelectorAll("article:not(." + CSS.escape(tag) + ")")
-                for (let not_match of not_matching) { not_match.classList.add("is-hidden") }
-            }
-            else {
-                const all_articles = getElementsByClassName("card")
-                for (let article in all_articles) {article.classList.remove("is-hidden")}
+                function filterByTag(org) {
+                    if (org.tags.includes(tag)) {
+                        return true
+                    }
+                }
+                const orgsFiltered = arr.filter(filterByTag)
+                console.log(orgsFiltered)
+                const html = orgsFiltered.map(match =>
+                    `<article class="card"><div><h2 class="card-title">${match.name}</div><div class="card-image"><img src="./images/twit_pics/${match.screen_name}.jpeg"></div><div class="content"><p>${match.short_description}</p></div><div class="tags"></div><div class="links-container"><a href="${match.url}"><div class="links">Website</div></a><a href="https://twitter.com/${match.screen_name}"><div class="links">Twitter</div></a></div></article>`
+                ).join('');
+                issues_list.innerHTML = ''
+                org_list.innerHTML = html
             }
         }
+
+        // search box
         const search_input = document.getElementById('org_input')
-        const org_list = document.getElementById('cards')
-        // get matches to current text input
+  
         function searchOrgs(searchText) {
             let matches = arr.filter(org => {
                 const regex = new RegExp(`${searchText}`, 'gi');
                 return org.name.match(regex) || org.short_description.match(regex)
-                
             });
             
             if (searchText.length === 0) {
                 matches = [];
                 org_list.innerHTML = '';
-
+                window.history.pushState({},document.title,"/")
+                fetchTagsFromOrgs()
             };
             outputHTML(matches);
         };
         
         const outputHTML = matches => {
             if (matches.length > 0) {
+                const issues_div = document.getElementById('issues')
                 const html = matches.map(match => 
                     `<article class="card"><div><h2 class="card-title">${match.name}</div><div class="card-image"><img src="./images/twit_pics/${match.screen_name}.jpeg"></div><div class="content"><p>${match.short_description}</p></div><div class="tags"></div><div class="links-container"><a href="${match.url}"><div class="links">Website</div></a><a href="https://twitter.com/${match.screen_name}"><div class="links">Twitter</div></a></div></article>`
                 ).join('');
-
+                issues_div.innerHTML = ''
                 org_list.innerHTML = html;
             }
         }

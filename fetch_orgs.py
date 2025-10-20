c6039adcbfaf1c783d0267da5bd5433a62594f2b
Willie Thompson <willielaredo@gmail.com>
2025-10-19 21:02:54 -0400

    use main source files

--- fetch_orgs.py
+++ fetch_orgs.py
@@ -11,10 +11,10 @@ supabase: Client = create_client(url, key)
 
 orgs_data = supabase.from_("orgs_json").select("*").execute()
 orgs_json = orgs_data.data[0]
-with open("orgs_.json", "w") as file:
+with open("orgs.json", "w") as file:
     json.dump(orgs_json["json_build_object"], file, indent=4)
 
 tags_data = supabase.from_("tag_list").select("*").execute()
 tags_json = tags_data.data[0]
-with open("tags_.json", "w") as file:
+with open("tags.json", "w") as file:
     json.dump(tags_json["json_build_object"], file, indent=4)
